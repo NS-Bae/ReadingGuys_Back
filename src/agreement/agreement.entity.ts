16a70aaf6297a1e21aa9cbca8971ba1f6e0ae6fb
NSB <qw9711@gmail.com>
2025-12-02 14:45:26 +0900

    DB클라우드 적재이후 일부사항 수정

--- src/agreement/agreement.entity.ts
+++ src/agreement/agreement.entity.ts
@@ -6,28 +6,16 @@ import { User } from "../users/users.entity";
 @Entity( 'TermsAgreement' )
 @Index('idx_user_terms', ['hashedUserId', 'termsType'])
 export class TermsAgreement {
-<<<<<<< HEAD
   @PrimaryGeneratedColumn({ name: 'ID', type: 'bigint' })
   id: string;
-=======
-  @PrimaryGeneratedColumn({ type: 'bigint' })
-  id: number;
->>>>>>> 7ca3b6ce1ed9d640b63e3deb0f695ac97bcab744
 
   @Column({ name: 'HashedUserID', type: 'varchar', length: 255, nullable: false })
   hashedUserId: string;
 
-<<<<<<< HEAD
   @Column({ name: 'TermsType', type: 'enum', enum: TermsTypes, nullable: false })
   termTypes: TermsTypes;
 
   @Column({ name: 'Version', type: 'varchar', length: 10, nullable: false })
-=======
-  @Column({ type: 'enum', enum: TermsTypes, nullable: false })
-  termsType: TermsTypes;
-
-  @Column({ type: 'varchar', length: 20, nullable: false })
->>>>>>> 7ca3b6ce1ed9d640b63e3deb0f695ac97bcab744
   version: string;
 
   @Column({ name: 'Agreed', type: 'boolean', nullable: false })
