a7659009c5809d88108692bc94e5144a706cf626
NSB <qw9711@gmail.com>
2026-08-29 11:11:50 +0900

    firebase 개선

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -3,9 +3,10 @@ import { FirebaseService } from "./firebase.service";
 import { FirebaseController } from './firebase.controller'
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { FcmDeviceToken } from "./fcmDeviceToken.entity";
+import { User } from "../users/users.entity";
 
 @Module({
-  imports:[TypeOrmModule.forFeature([FcmDeviceToken])],
+  imports:[TypeOrmModule.forFeature([FcmDeviceToken, User])],
   providers : [FirebaseService],
   exports : [FirebaseService],
   controllers : [FirebaseController],
